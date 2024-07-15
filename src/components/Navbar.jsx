eb92f5ef7463f5332a2704e487cceaef471aaaf8
Yashvardhan Arora <yash22arora@gmail.com>
2024-07-15 23:35:54 +0530

    Complete Hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { useState } from "react";
+
+const NAV_ITEMS = [
+  {
+    id: "home",
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: "about",
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    id: "courses",
+    title: "Courses",
+    path: "/courses",
+  },
+  {
+    id: "faq",
+    title: "FAQ",
+    path: "/faq",
+  },
+];
+
+const Navbar = () => {
+  const [activeNavvLink, setActiveNavLink] = useState("home");
+  return (
+    <nav className="py-10 px-20 flex flex-row justify-between items-center w-full relative z-10 text-sm">
+      <img src={logo} alt="logo" className="w-16 h-full aspect-auto" />
+      <div className="flex flex-row gap-8">
+        <ul className="flex justify-between gap-8 items-center">
+          {NAV_ITEMS.map((item) => (
+            <Link
+              key={item.title}
+              to={item.path}
+              className={
+                "text-white hover:text-gray-200 cursor-pointer hover:underline hover:underline-offset-4" +
+                (activeNavvLink === item.id
+                  ? " font-bold underline-offset-4 underline"
+                  : "")
+              }
+            >
+              {item.title}
+            </Link>
+          ))}
+        </ul>
+        <button className="p-2 px-6 text-sm">Contact Us</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+import heroImg from "../assets/hero.png";
+import Navbar from "../components/Navbar";
+import { FaArrowRight } from "react-icons/fa6";
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col items-center z-10 relative">
+      <Navbar />
+      <div className="hero h-[calc(100vh_-_160px)] flex place-items-center">
+        <img
+          src={heroImg}
+          className="w-full absolute left-0 opacity-80 top-0 z-0"
+          alt="hero"
+        />
+        <div className="w-1/2 mx-auto flex flex-col items-center gap-8 text-center z-10">
+          <h1 className="text-6xl font-bold text-center text-white">
+            Where the joy of learn meets the power of community
+          </h1>
+          <p>
+            Dive deep in immersive, interactive small groups. Expand horizons,
+            engage in <br /> discussions, and elevate your learning journey with
+            us.
+          </p>
+          <button>Start your travel</button>
+          <Link
+            to="/"
+            className="text-sm hover:text-white text-[#DDD6FE] flex flex-row gap-2 place-items-center"
+          >
+            <span>Watch a video</span>
+            <FaArrowRight />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
